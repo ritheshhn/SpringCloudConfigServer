d858c40f2776fb5a0a5147a74cdac735f8aa1f01
RitheshNagaraj <rithesh.nagaraj@thantrick.in>
2024-07-30 20:15:02 +0530

    Initial commit

---
+++ src/test/java/com/thantrick/springcloud_configserver_demo/SpringCloudConfigServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thantrick.springcloud_configserver_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
